6d436bbcfed46c599954f56f2dde50953447f8a2
Harshit Pradhan <73273384+Harshitpradhan7@users.noreply.github.com>
2024-04-13 13:07:25 +0000

    DataTypes

---
+++ 01_Basics/dataTypes.js
@@ -0,0 +1,20 @@
+const a = 2
+let b = 3
+var c = 4 // Not prefer to use used in old codes:
+ 
+
+console.log(a,b,c);
+
+// Data types
+
+// number
+// bigint
+// string
+// boolean
+// null => standalone value =type object
+// undefined => UNDEFINED
+// symbol => 
+
+
+console.log(typeof(null));
+console.log(typeof(undefined));
